b0fefbc09485fe884aab0784a177475ed32f4b77
Salih Emir <salihemirguner@gmail.com>
2025-06-01 00:33:18 +0300

    Update FSM_advanced.v

--- additional/FSM_advanced.v
+++ additional/FSM_advanced.v
@@ -10,18 +10,24 @@ module Sprite_FSM (
     output reg attack_flag
 );
 
-    localparam [2:0] 
-        S_IDLE = 3'd0,
-        S_Backward = 3'd1,
-        S_Forward = 3'd2,
-        S_Attack_start = 3'd3,
-        S_Attack_active = 3'd4,
-        S_Attack_recovery = 3'd5;
+    localparam [3:0] 
+        S_IDLE             = 4'd0,
+        S_Backward         = 4'd1,
+        S_Forward          = 4'd2,
+        S_Attack_start     = 4'd3,
+        S_Attack_active    = 4'd4,
+        S_Attack_recovery  = 4'd5,
+        S_DirAtk_start     = 4'd6,
+        S_DirAtk_active    = 4'd7,
+        S_DirAtk_recovery  = 4'd8;
 
     localparam ATTACK_START_FRAMES = 5;
     localparam ATTACK_ACTIVE_FRAMES = 2;
     localparam ATTACK_RECOVERY_FRAMES = 16;
-
+    localparam DIRATK_START_FRAMES = 4;
+    localparam DIRATK_ACTIVE_FRAMES = 3;
+    localparam DIRATK_RECOVERY_FRAMES = 15;
+    
     reg [5:0] frame_counter;
 
     always @(posedge clk) begin
@@ -32,35 +38,50 @@ module Sprite_FSM (
             case (state)
                 S_IDLE: begin
                     frame_counter <= 0;
-                  if (left && ~right)
-                        state <= S_Backward;
-                  else if (right && ~left)
-                        state <= S_Forward;
-                  else if (attack && ~left && ~right)
+                    if (left && ~right && attack)
+                        state <= S_DirAtk_start;
+                    else if (right && ~left && attack)
+                        state <= S_DirAtk_start;
+                    else if (attack && ~left && ~right)
                         state <= S_Attack_start;
-                  else state = S_IDLE;
+                    else if (left && ~right)
+                        state <= S_Backward;
+                    else if (right && ~left)
+                        state <= S_Forward;
+                    else
+                        state <= S_IDLE;
                 end
 
                 S_Backward: begin
                     frame_counter <= 0;
-                  if (left && ~right)
-                        state <= S_Backward;
-                  else if (right && ~left)
-                        state <= S_Forward;
-                  else if (attack && ~left && ~right)
+                    if (left && ~right && attack)
+                        state <= S_DirAtk_start;
+                    else if (right && ~left && attack)
+                        state <= S_DirAtk_start;
+                    else if (attack && ~left && ~right)
                         state <= S_Attack_start;
-                  else state = S_IDLE;
+                    else if (left && ~right)
+                        state <= S_Backward;
+                    else if (right && ~left)
+                        state <= S_Forward;
+                    else
+                        state <= S_IDLE;
                 end
 
                 S_Forward: begin
                     frame_counter <= 0;
-                  if (left && ~right)
-                        state <= S_Backward;
-                  else if (right && ~left)
-                        state <= S_Forward;
-                  else if (attack && ~left && ~right)
+                    if (left && ~right && attack)
+                        state <= S_DirAtk_start;
+                    else if (right && ~left && attack)
+                        state <= S_DirAtk_start;
+                    else if (attack && ~left && ~right)
                         state <= S_Attack_start;
-                  else state = S_IDLE;
+                    else if (left && ~right)
+                        state <= S_Backward;
+                    else if (right && ~left)
+                        state <= S_Forward;
+                    else
+                        state <= S_IDLE;
                 end
 
                 S_Attack_start: begin
@@ -90,6 +111,34 @@ module Sprite_FSM (
                     end
                 end
 
+                // Directional attack phases
+                S_DirAtk_start: begin
+                    if (frame_counter >= DIRATK_START_FRAMES - 1) begin
+                        state <= S_DirAtk_active;
+                        frame_counter <= 0;
+                    end else begin
+                        frame_counter <= frame_counter + 1;
+                    end
+                end
+                
+                S_DirAtk_active: begin
+                    if (frame_counter >= DIRATK_ACTIVE_FRAMES - 1) begin
+                        state <= S_DirAtk_recovery;
+                        frame_counter <= 0;
+                    end else begin
+                        frame_counter <= frame_counter + 1;
+                    end
+                end
+                
+                S_DirAtk_recovery: begin
+                    if (frame_counter >= DIRATK_RECOVERY_FRAMES - 1) begin
+                        state <= S_IDLE;
+                        frame_counter <= 0;
+                    end else begin
+                        frame_counter <= frame_counter + 1;
+                    end
+                end
+
                 default: begin
                     state <= S_IDLE;
                     frame_counter <= 0;
@@ -107,11 +156,16 @@ module Sprite_FSM (
         case(state)
             S_Backward, S_Forward: begin
                 move_flag = 1;
-                directional_attack_flag = attack;
+                directional_attack_flag = attack;            
             end
             S_Attack_start,
             S_Attack_active: begin
                 attack_flag = 1;
+            end
+            S_DirAtk_start,
+            S_DirAtk_active: begin
+                attack_flag = 1;
+                directional_attack_flag = 1;
             end
         endcase
     end
