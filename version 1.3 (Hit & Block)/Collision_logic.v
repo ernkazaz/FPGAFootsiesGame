2801fcfa7418ea2c928fadec3ff2ab817492fbd5
Salih Emir <salihemirguner@gmail.com>
2025-06-01 15:21:19 +0300

    Update Collision_logic.v

--- version 1.3 (Hit & Block)/Collision_logic.v
+++ version 1.3 (Hit & Block)/Collision_logic.v
@@ -1,22 +1,43 @@
 module Collision_logic (
-    input [9:0] hitbox_x1_p1, hitbox_x2_p1, hitbox_y1_p1, hitbox_y2_p1,
-    input       hitbox_active_p1,
-    input       attack_flag_p1,
-    input       dir_attack_flag_p1,
+    input [9:0] attacker_hitbox_x1,
+    input [9:0] attacker_hitbox_x2,
+    input [9:0] attacker_hitbox_y1,
+    input [9:0] attacker_hitbox_y2,
+    input       attacker_hitbox_active,
+    input       attacker_attack_flag,
+    input       attacker_diratk_flag,
 
-    input [9:0] hurtbox_x1_p2, hurtbox_x2_p2, hurtbox_y1_p2, hurtbox_y2_p2,
-    input       hurtbox_active_p2,
-    input       is_blocking_p2,
+    input [9:0] target_hurtbox_x1,
+    input [9:0] target_hurtbox_x2,
+    input [9:0] target_hurtbox_y1,
+    input [9:0] target_hurtbox_y2,
+    input       target_hurtbox_active,
+    input       target_is_blocking,
 
-    output      got_hit_p2,
-    output      got_blocked_p2
+    output reg  got_hit_target,
+    output reg  got_blocked_target
 );
 
-    wire overlapping = hitbox_active_p1 && hurtbox_active_p2 &&
-        (hitbox_x1_p1 < hurtbox_x2_p2 && hitbox_x2_p1 > hurtbox_x1_p2) &&
-        (hitbox_y1_p1 < hurtbox_y2_p2 && hitbox_y2_p1 > hurtbox_y1_p2);
+    wire x_overlap = (attacker_hitbox_x1 < target_hurtbox_x2) &&
+                     (attacker_hitbox_x2 > target_hurtbox_x1);
+    wire y_overlap = (attacker_hitbox_y1 < target_hurtbox_y2) &&
+                     (attacker_hitbox_y2 > target_hurtbox_y1);
+    wire hit_detected = attacker_hitbox_active && target_hurtbox_active &&
+                        attacker_attack_flag && x_overlap && y_overlap;
 
-    assign got_hit_p2 = overlapping && attack_flag_p1 && ~is_blocking_p2;
-    assign got_blocked_p2 = overlapping && dir_attack_flag_p1 && is_blocking_p2;
+    always @(*) begin
+        if (hit_detected) begin
+            if (target_is_blocking) begin
+                got_hit_target = 0;
+                got_blocked_target = 1;
+            end else begin
+                got_hit_target = 1;
+                got_blocked_target = 0;
+            end
+        end else begin
+            got_hit_target = 0;
+            got_blocked_target = 0;
+        end
+    end
 
 endmodule
